9792699a3ccbae60b450aeae79deadc761860ea1
Alex <alexandra.papadopoulou@bayes.gg>
2023-11-17 15:21:23 +0100

    chore: JTD-98 remove small comment

--- src/main/java/com/github/actions/server/test/GithubActionsServerTestApplication.java
+++ src/main/java/com/github/actions/server/test/GithubActionsServerTestApplication.java
@@ -9,5 +9,4 @@ public class GithubActionsServerTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GithubActionsServerTestApplication.class, args);
     }
-
 }
