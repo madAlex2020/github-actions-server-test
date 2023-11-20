9884f605fe3761716bdc36b63ee0f24f9aee82f3
Alex <alexandra.papadopoulou@bayes.gg>
2023-11-20 09:43:19 +0100

    chore: JTD-98 comment

--- src/main/java/com/github/actions/server/test/GithubActionsServerTestApplication.java
+++ src/main/java/com/github/actions/server/test/GithubActionsServerTestApplication.java
@@ -9,4 +9,5 @@ public class GithubActionsServerTestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GithubActionsServerTestApplication.class, args);
     }
+    //test
 }
