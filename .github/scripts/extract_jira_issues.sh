0d13fabd004758cb13357826b0ef126ce463a1e4
Alex <alexandra.papadopoulou@bayes.gg>
2023-11-20 10:23:48 +0100

    chore: JTD-98 still fix issue, previous release not found

--- .github/scripts/extract_jira_issues.sh
+++ .github/scripts/extract_jira_issues.sh
@@ -48,8 +48,12 @@ EOF
 }
 
 # Determine the range of commits to check
-TAG_NAME=$1
-PREVIOUS_TAG=$(git tag --sort=-creatordate | grep -B 1 ${TAG_NAME} | head -n 1)
+TAG_NAME=${GITHUB_REF##*/}
+
+# Fetch all tags and sort them by date
+git fetch --tags
+git tag --sort=-v:refname | grep -A 1 $TAG_NAME | tail -n 1
+PREVIOUS_TAG=$(git tag --sort=-v:refname | grep -A 1 $TAG_NAME | tail -n 1)
 
 if [ -z "$PREVIOUS_TAG" ] || [ "$PREVIOUS_TAG" = "$TAG_NAME" ]; then
     echo "No previous tag found. Examining all commits."
@@ -58,6 +62,7 @@ else
     echo "Examining commits from $PREVIOUS_TAG to $TAG_NAME"
     COMMIT_RANGE="${PREVIOUS_TAG}..${TAG_NAME}"
 fi
+
 
 # Extract Jira issue codes and update issues in Jira
 git log $COMMIT_RANGE --pretty=format:"%s" | grep -oE '[A-Z]+-[0-9]+' | sort | uniq | while read issue; do
