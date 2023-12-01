a1fa7cb1a16332d4f53ec6de12450d68adc4a528
Alex <alexandra.papadopoulou@bayes.gg>
2023-12-01 17:09:43 +0100

    JTD-98 send tasks in bulk

--- .github/scripts/extract_jira_issues.sh
+++ .github/scripts/extract_jira_issues.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
+declare -A issue_lists
+issue_lists["IP"]=()
+issue_lists["JTD"]=()
+issue_lists["LDST"]=()
+
 # Function to trigger Jira Automation Webhook
 trigger_jira_automation() {
-    local issue="$1"
+    local issues=("${!1}")
     local webhook_url="$2"
 
-    local payload="{\"issues\": [\"$issue\"]}"
+    if [[ ${#issues[@]} -gt 0 ]]; then
+        local issues_json=$(printf '"%s",' "${issues[@]}" | sed 's/,$//')
+        issues_json="[$issues_json]"
 
-    # Make a POST request to the Jira Automation Webhook
-    curl --request POST \
-         --url "$webhook_url" \
-         --header 'Content-Type: application/json' \
-         --data "$payload"
+        local payload="{\"issues\": $issues_json}"
+
+        curl --request POST \
+             --url "$webhook_url" \
+             --header 'Content-Type: application/json' \
+             --data "$payload"
+    fi
 }
 
 # Determine the range of commits to check
@@ -33,16 +42,31 @@ fi
 git log "$COMMIT_RANGE" --pretty=format:"%s" | grep -oE '[A-Z]+-[0-9]+' | sort | uniq | while read -r issue; do
     case "${issue%%-*}" in
         "IP")
-            trigger_jira_automation "$issue" "${JIRA_WEBHOOK_IP}"
+            issue_lists["IP"]+=("$issue")
             ;;
         "JTD")
-            trigger_jira_automation "$issue" "${JIRA_WEBHOOK_JTD}"
+            issue_lists["JTD"]+=("$issue")
             ;;
         "LDST")
-            trigger_jira_automation "$issue" "${JIRA_WEBHOOK_LDST}"
+            issue_lists["LDST"]+=("$issue")
             ;;
         *)
             echo "No webhook configured for this issue type: $issue"
+            ;;
+    esac
+done
+
+# Trigger webhooks for each issue list
+for project_key in "${!issue_lists[@]}"; do
+    case "$project_key" in
+        "IP")
+            trigger_jira_automation issue_lists["IP"][@] "${JIRA_WEBHOOK_IP}"
+            ;;
+        "JTD")
+            trigger_jira_automation issue_lists["JTD"][@] "${JIRA_WEBHOOK_JTD}"
+            ;;
+        "LDST")
+            trigger_jira_automation issue_lists["LDST"][@] "${JIRA_WEBHOOK_LDST}"
             ;;
     esac
 done
