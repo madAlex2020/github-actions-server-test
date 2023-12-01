ae331f993c60a1968e1ef61e7f05d61bcfdcdcab
Alex <alexandra.papadopoulou@bayes.gg>
2023-12-01 12:35:26 +0100

    LDST-358 add more webhooks

--- .github/scripts/extract_jira_issues.sh
+++ .github/scripts/extract_jira_issues.sh
@@ -1,52 +1,48 @@
 #!/bin/bash
 
-WEBHOOK_URL=${JIRA_WEBHOOK}
-issue_keys=()
+# Function to trigger Jira Automation Webhook
+trigger_jira_automation() {
+    local issue="$1"
+    local webhook_url="$2"
 
+    local payload="{\"issues\": [\"$issue\"]}"
+
+    # Make a POST request to the Jira Automation Webhook
+    curl --request POST \
+         --url "$webhook_url" \
+         --header 'Content-Type: application/json' \
+         --data "$payload"
+}
 
 # Determine the range of commits to check
 TAG_NAME=${GITHUB_REF##*/}
 
 # Fetch all tags and sort them by date
 git fetch --tags
-git tag --sort=-v:refname | grep -A 1 $TAG_NAME | tail -n 1
-PREVIOUS_TAG=$(git tag --sort=-v:refname | grep -A 1 $TAG_NAME | tail -n 1)
+PREVIOUS_TAG=$(git tag --sort=-v:refname | grep -A 1 "$TAG_NAME" | tail -n 1)
 
-if [ -z "$PREVIOUS_TAG" ] || [ "$PREVIOUS_TAG" = "$TAG_NAME" ]; then
-    echo "No previous tag found. Examining all commits."
-    COMMIT_RANGE=''
-else
+COMMIT_RANGE=''
+if [[ -n "$PREVIOUS_TAG" ]] && [[ "$PREVIOUS_TAG" != "$TAG_NAME" ]]; then
     echo "Examining commits from $PREVIOUS_TAG to $TAG_NAME"
     COMMIT_RANGE="${PREVIOUS_TAG}..${TAG_NAME}"
-fi
-
-
-# Extract Jira issue codes
-while read issue; do
-    echo "Updating Jira issue: $issue"
-    issue_keys+=("$issue")
-    echo "Issue keys: ${issue_keys[@]}"
-done < <(git log $COMMIT_RANGE --pretty=format:"%s" | grep -oE '[A-Z]+-[0-9]+' | sort | uniq)
-
-# Function to trigger Jira Automation Webhook
-trigger_jira_automation() {
-    # Construct JSON array of issues
-    local issues_array=$(printf '%s\n' "${issue_keys[@]}" | jq -R . | jq -cs .)
-
-    # Create JSON payload with "issues" field
-    local payload="{\"issues\": $issues_array}"
-
-    # Make a POST request to the Jira Automation Webhook
-    curl --request POST \
-         --url "$WEBHOOK_URL" \
-         --header 'Content-Type: application/json' \
-         --data "$payload"
-}
-
-# Check if there are any issues to update
-if [[ ! -z "${issue_keys[*]}" ]]; then
-    echo "Triggering automation for Jira issues"
-    trigger_jira_automation
 else
-    echo "No Jira issues found to update."
+    echo "No previous tag found. Examining all commits."
 fi
+
+# Extract Jira issue codes and decide which webhook to use
+git log "$COMMIT_RANGE" --pretty=format:"%s" | grep -oE '[A-Z]+-[0-9]+' | sort | uniq | while read -r issue; do
+    case "${issue%%-*}" in
+        "IP")
+            trigger_jira_automation "$issue" "${JIRA_WEBHOOK_IP}"
+            ;;
+        "JTD")
+            trigger_jira_automation "$issue" "${JIRA_WEBHOOK_JTD}"
+            ;;
+        "LDST")
+            trigger_jira_automation "$issue" "${JIRA_WEBHOOK_LDST}"
+            ;;
+        *)
+            echo "No webhook configured for this issue type: $issue"
+            ;;
+    esac
+done
